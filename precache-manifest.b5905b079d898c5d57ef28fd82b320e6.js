84e3ed8b5b052fbc6e71554c3a5b49fafb5742bc
Robin <mehmet.ucar@imediapro.co.uk>
2020-09-25 20:17:46 +0100

    Updates

---
+++ precache-manifest.b5905b079d898c5d57ef28fd82b320e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b616d6aac68824c7c108ec43cd9b4d",
+    "url": "/It-Logger/index.html"
+  },
+  {
+    "revision": "c871ae750731e5aff87c",
+    "url": "/It-Logger/static/css/2.53159467.chunk.css"
+  },
+  {
+    "revision": "ecebe59c325933d0384c",
+    "url": "/It-Logger/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c871ae750731e5aff87c",
+    "url": "/It-Logger/static/js/2.bf1bec31.chunk.js"
+  },
+  {
+    "revision": "c08e00e85e39ffd17f6ab18b75a9eabc",
+    "url": "/It-Logger/static/js/2.bf1bec31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecebe59c325933d0384c",
+    "url": "/It-Logger/static/js/main.f04bff13.chunk.js"
+  },
+  {
+    "revision": "cd85de46df8d949c04b6",
+    "url": "/It-Logger/static/js/runtime-main.3eb54e87.js"
+  }
+]);
